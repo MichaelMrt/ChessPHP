04fec833a91635daaa73286b4946cac6d8ff4381
MichaelMrt03 <116624593+MichaelMrt03@users.noreply.github.com>
2024-02-06 18:06:55 +0100

    print 8x8 array

--- src/controller.php
+++ src/controller.php
@@ -1,4 +1,6 @@
 <?php
+set_time_limit(10);
+
 require_once("logic/logic.php");
 
 echo "<H1>Controller</H1>";
--- src/logic/logic.php
+++ src/logic/logic.php
@@ -7,5 +7,19 @@ class Logic
     {
         echo "Logic constructed";
         new Pawn("w");
+        echo "<pre>";
+        print_r($this->create_board());
+        echo "</pre>";
+    }
+
+    private function create_board()
+    {
+        #Creates an 8x8 array
+        for ($x = 1; $x < 9; $x++) {
+            for ($y = 0; $y < 9; $y++) {
+                $chessboard[$x][$y] = "";
+            }
+        }
+        return $chessboard;
     }
 }
