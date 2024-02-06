f0e1a8c51ccf639ea273c4a0da8233e08874a40b
MichaelMrt03 <116624593+MichaelMrt03@users.noreply.github.com>
2024-02-06 21:11:34 +0100

    Add simple move function

--- src/chesspieces/pawn.php
+++ src/chesspieces/pawn.php
@@ -3,5 +3,10 @@ require_once("chess_piece.php");
 
 class Pawn extends ChessPiece
 {
+    function move($chessboard){
+        $chessboard[1][1] = new Pawn("white"); 
+        return $chessboard;
+    }
 
+    
 }
