ef520952491c199b5a162f0bff2a53e1bff59c60
MichaelMrt03 <116624593+MichaelMrt03@users.noreply.github.com>
2024-02-08 15:20:36 +0100

    implemented JsonSerializable for pawns

--- src/logic/chesspieces/pawn.php
+++ src/logic/chesspieces/pawn.php
@@ -1,10 +1,18 @@
 <?php
 require_once("chess_piece.php");
 
-class Pawn extends ChessPiece
+class Pawn extends ChessPiece implements JsonSerializable
 {
     function check_move_legal()
     {
         
+    }
+
+    public function jsonSerialize() {
+        return [
+            'x' => $this->x,
+            'y' => $this->x,
+            'color' => $this->y,
+        ];
     }
 }
