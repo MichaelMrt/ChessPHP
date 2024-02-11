2f2050a90cf086d3ec4d2c96c19e96e1f2f12f18
Aman Ur-Rehman <urrehman.aman@outlook.de>
2024-02-11 14:24:00 +0100

    basic struture for check_move_legal function built

--- src/logic/chesspieces/pawn.php
+++ src/logic/chesspieces/pawn.php
@@ -3,9 +3,34 @@ require_once("chess_piece.php");
 
 class Pawn extends ChessPiece implements JsonSerializable
 {
-    function check_move_legal()
+    function check_move_legal($chessboard, $move_to_x, $move_to_y)
     {
+        # Coordinates from the current Piece position
+        $current_x = $this->x;
+        $current_y = $this->y;
         
+        #check if there is something on the move_to_field
+        if($chessboard[$move_to_x][$move_to_y] == ""){
+            #check if pawn is moving two fields
+            if(twofiedsmoving){
+                #check if pawn is on the right field to be allowed to move two fields
+                if(pawnonrightfield?){
+                    return true;
+                }else{
+                    return false;
+                }
+            #check if pawn is only moving one field
+            }elseif(onefieldmoving){
+                return true;
+            }else{
+                return false
+            }
+        #check if pawn is moving diagonal
+        }elseif(){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     public function jsonSerialize() {
