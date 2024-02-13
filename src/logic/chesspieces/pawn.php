5c0c346229dbd613356a9a611815c66652bb31d2
MichaelMrt03 <116624593+MichaelMrt03@users.noreply.github.com>
2024-02-13 14:13:33 +0100

    formatted check_move_legal func

--- src/logic/chesspieces/pawn.php
+++ src/logic/chesspieces/pawn.php
@@ -19,17 +19,23 @@ class Pawn extends ChessPiece
         #check if there is something on the move_to_field
         if($chessboard[$move_to_x][$move_to_y] == ""){
             #check if pawn is moving two fields forwards, check which color is moving, according to the color check if it is the start position of the pawn, x position should stay the same
-            if(((($current_y-2 == $move_to_y) and ($this->color == "black") and ($current_y == 7))  or  (($current_y+2 == $move_to_y) and ($this->color == "white") and ($current_y == 2))) and $current_x == $move_to_x){
+            if(((($current_y-2 == $move_to_y) and ($this->color == "black") and ($current_y == 7))  or  
+                (($current_y+2 == $move_to_y) and ($this->color == "white") and ($current_y == 2))) and 
+                  $current_x == $move_to_x){
                 return true;
             #check if pawn is only moving one field forwards
-            }elseif(((($current_y-1 == $move_to_y) and ($this->color == "black"))  or  (($current_y+1 == $move_to_y) and ($this->color == "white"))) and $current_x == $move_to_x){
-                return true;
+            }elseif(((($current_y-1 == $move_to_y) and ($this->color == "black"))  or  
+                     (($current_y+1 == $move_to_y) and ($this->color == "white"))) and 
+                       $current_x == $move_to_x){
+                    return true;
             }else{
                 return false;
             }
         #check if pawn is moving one field diagonal
-        }elseif(((($current_y-1 == $move_to_y) and ($this->color == "black"))  or  (($current_y+1 == $move_to_y) and ($this->color == "white"))) and (($current_x+1 == $move_to_x) or ($current_x-1 == $move_to_x))){
-            return true;
+        }elseif(((($current_y-1 == $move_to_y) and ($this->color == "black"))  or  
+                 (($current_y+1 == $move_to_y) and ($this->color == "white"))) and 
+                 (($current_x+1 == $move_to_x) or ($current_x-1 == $move_to_x))){
+                return true;
         }else{
             return false;
         }
