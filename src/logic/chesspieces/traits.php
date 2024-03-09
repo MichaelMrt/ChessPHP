99049d67b819e69f933bb002410e9d0cd74cd917
MichaelMrt03 <116624593+MichaelMrt03@users.noreply.github.com>
2024-03-10 00:05:51 +0100

    implemented the check_legal_rookmove trait

--- src/logic/chesspieces/traits.php
+++ src/logic/chesspieces/traits.php
@@ -1,8 +1,20 @@
 <?php
 trait RookTrait{
-    public function check_legal_rookmove():bool
+    public function check_legal_rookmove(int $current_x, int $current_y, int $move_to_x, int $move_to_y):bool
     {
-        return true;
+        #check if its horizontally
+        if($current_y==$move_to_y && $current_x != $move_to_x){
+            # toDo: check if there is a piece on the way
+            return true;
+        }
+
+        #check if its vertically
+        if($current_x==$move_to_x && $current_y != $move_to_y){
+            # toDo: check if there is a piece on the way
+            return true;
+        }
+
+        return false;
     }
 }
 ?>
