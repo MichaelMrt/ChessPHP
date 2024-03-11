80b661f0a1bde6028859c461ee54cda6835a7f88
MichaelMrt03 <116624593+MichaelMrt03@users.noreply.github.com>
2024-03-11 21:36:57 +0100

    restructured knight check_move_legal

--- src/logic/chesspieces/knight.php
+++ src/logic/chesspieces/knight.php
@@ -15,9 +15,24 @@ class Knight extends ChessPiece
 
     function check_move_legal(mixed $chessboard, int $move_to_x, int $move_to_y):bool
     {
-         # 1-up, 2-right jump
+         # 1-up and 2-right jump
+         # 1-up and 2-left jump
+         # 1-down and 2-right jump
+         # 1-down and 2-left jump
+         # 2-up and 1-right jump
+         # 2-up and 1-left jump
+         # 2-down and 1-right jump
+         # 2-down and 1-left jump
+
          # check if moving in legal pattern, then check if its moving to a field with a piece and if its opposite colors
-         if($move_to_y==$this->y+1 && $move_to_x==$this->x+2){
+         if(($move_to_y==$this->y+1 && $move_to_x==$this->x+2)||
+             $move_to_y==$this->y+1 && $move_to_x==$this->x-2||
+             $move_to_y==$this->y-1 && $move_to_x==$this->x+2||
+             $move_to_y==$this->y-1 && $move_to_x==$this->x-2||
+             $move_to_y==$this->y+2 && $move_to_x==$this->x+1||
+             $move_to_y==$this->y+2 && $move_to_x==$this->x-1||
+             $move_to_y==$this->y-2 && $move_to_x==$this->x+1||
+             $move_to_y==$this->y-2 && $move_to_x==$this->x-1){
           if(is_a($chessboard[$move_to_x][$move_to_y],'Chesspiece')){
               if($chessboard[$this->x][$this->y]->get_color()!=$chessboard[$move_to_x][$move_to_y]->get_color()){
                 return true;
@@ -25,84 +40,6 @@ class Knight extends ChessPiece
           }else{
             return true;
           }
-        }
-        # 1-up, 2-left jump
-        if($move_to_y==$this->y+1 && $move_to_x==$this->x-2){
-          if(is_a($chessboard[$move_to_x][$move_to_y],'Chesspiece')){
-            if($chessboard[$this->x][$this->y]->get_color()!=$chessboard[$move_to_x][$move_to_y]->get_color()){
-              return true;
-            }
-        }else{
-          return true;
-        }
-        }
-        # 1-down, 2-right jump
-        if($move_to_y==$this->y-1 && $move_to_x==$this->x+2){
-          if(is_a($chessboard[$move_to_x][$move_to_y],'Chesspiece')){
-            if($chessboard[$this->x][$this->y]->get_color()!=$chessboard[$move_to_x][$move_to_y]->get_color()){
-              return true;
-            }
-        }else{
-          return true;
-        }
-        }
-        # 1-down, 2-left jump
-        if($move_to_y==$this->y-1 && $move_to_x==$this->x-2){
-          if(is_a($chessboard[$move_to_x][$move_to_y],'Chesspiece')){
-            if($chessboard[$this->x][$this->y]->get_color()!=$chessboard[$move_to_x][$move_to_y]->get_color()){
-              return true;
-            }
-        }else{
-          if(is_a($chessboard[$move_to_x][$move_to_y],'Chesspiece')){
-            if($chessboard[$this->x][$this->y]->get_color()!=$chessboard[$move_to_x][$move_to_y]->get_color()){
-              return true;
-            }
-        }else{
-          return true;
-        }
-        }
-        }
-
-
-        # 2-up, 1-right jump
-        if($move_to_y==$this->y+2 && $move_to_x==$this->x+1){
-          if(is_a($chessboard[$move_to_x][$move_to_y],'Chesspiece')){
-            if($chessboard[$this->x][$this->y]->get_color()!=$chessboard[$move_to_x][$move_to_y]->get_color()){
-              return true;
-            }
-        }else{
-          return true;
-        }
-        }
-        # 2-up, 1-left jump
-        if($move_to_y==$this->y+2 && $move_to_x==$this->x-1){
-          if(is_a($chessboard[$move_to_x][$move_to_y],'Chesspiece')){
-            if($chessboard[$this->x][$this->y]->get_color()!=$chessboard[$move_to_x][$move_to_y]->get_color()){
-              return true;
-            }
-        }else{
-          return true;
-        }
-        }
-        # 2-down, 1-right jump
-        if($move_to_y==$this->y-2 && $move_to_x==$this->x+1){
-          if(is_a($chessboard[$move_to_x][$move_to_y],'Chesspiece')){
-            if($chessboard[$this->x][$this->y]->get_color()!=$chessboard[$move_to_x][$move_to_y]->get_color()){
-              return true;
-            }
-        }else{
-          return true;
-        }
-        }
-        # 2-down, 1-left jump
-        if($move_to_y==$this->y-2 && $move_to_x==$this->x-1){
-          if(is_a($chessboard[$move_to_x][$move_to_y],'Chesspiece')){
-            if($chessboard[$this->x][$this->y]->get_color()!=$chessboard[$move_to_x][$move_to_y]->get_color()){
-              return true;
-            }
-        }else{
-          return true;
-        }
         }
 
         $_SESSION['error'] = "<p class='error'>knights can't move like that</p>";
