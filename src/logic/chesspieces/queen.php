222494a79bac63031fad008a14375081849916d8
MichaelMrt03 <116624593+MichaelMrt03@users.noreply.github.com>
2024-03-11 20:51:41 +0100

    added queen check_move_legal

--- src/logic/chesspieces/queen.php
+++ src/logic/chesspieces/queen.php
@@ -1,6 +1,12 @@
 <?php
+require_once('rook.php');
+require_once('bishop.php');
 class Queen extends ChessPiece
 {
+
+    use RookTrait;
+    use BishopTrait;
+
     function __construct(String $color, int $x, int $y)
     {
       parent::__construct($color, $x, $y);
@@ -15,7 +21,11 @@ class Queen extends ChessPiece
 
     function check_move_legal(mixed $chessboard, int $move_to_x, int $move_to_y):bool
     {
+      if($this->check_legal_rookmove($chessboard, $this->x, $this->y,  $move_to_x,  $move_to_y) || $this->check_legal_bishopmove($chessboard, $this->x, $this->y,  $move_to_x,  $move_to_y)){
         return true;
+      }
+      $_SESSION['error'] = "<p class='error'>queens can't move like that</p>";
+      return false;
     }
 }
 ?>
