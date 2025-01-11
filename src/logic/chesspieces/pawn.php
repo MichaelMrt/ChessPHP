14f34d179d9b8e4d22eb31edf65f8edb931139d9
Michael <michael.martynov03@gmail.com>
2025-01-11 15:47:07 +0100

    Fixed a pawn bug and first work on en passant

--- src/logic/chesspieces/pawn.php
+++ src/logic/chesspieces/pawn.php
@@ -3,12 +3,15 @@ require_once("chess_piece.php");
 
 class Pawn extends ChessPiece
 {
+  public $check_enpassant = false;
+  public $enpassant_possible = false;
+  public $chessboard;
 
   function __construct(String $color, int $x, int $y)
   {
     parent::__construct($color, $x, $y);
     $this->type = 'pawn';
-
+    
     if ($color == 'white') {
       $this->icon = "<img src='../images/chesspieces/white-pawn.png' class='chesspiece'>";
     } elseif ($color == 'black') {
@@ -25,7 +28,8 @@ class Pawn extends ChessPiece
 
     return $this->check_moving_onesquare_forwards($current_x, $current_y, $move_to_x, $move_to_y) or
       $this->check_moving_twosquares_forwards($chessboard, $current_x, $current_y, $move_to_x, $move_to_y) or
-      $this->check_diagonal_move($chessboard, $current_x, $current_y, $move_to_x, $move_to_y);
+      $this->check_diagonal_move($chessboard, $current_x, $current_y, $move_to_x, $move_to_y) or
+      $this->check_enpassant($chessboard, $current_x, $current_y, $move_to_x, $move_to_y);
   }
 
 
@@ -34,7 +38,7 @@ class Pawn extends ChessPiece
     $is_white_move = $this->color == "white" && $current_y + 1 == $move_to_y;
     $is_black_move = $this->color == "black" && $current_y - 1 == $move_to_y;
     $same_file = $current_x == $move_to_x;
-    return $is_white_move || $is_black_move && $same_file;
+    return ($is_white_move || $is_black_move) && $same_file;
   }
 
 
@@ -43,17 +47,54 @@ class Pawn extends ChessPiece
     $is_white_move = $this->color == "white" && $current_y == 2 && $move_to_y == 4 && $chessboard[$current_x][$current_y + 1] == "";
     $is_black_move = $this->color == "black" && $current_y == 7 && $move_to_y == 5 && $chessboard[$current_x][$current_y - 1] == "";
     $same_file = $current_x == $move_to_x;
-    return $is_white_move || $is_black_move && $same_file;
+    $this->check_enpassant = true;
+    $this->chessboard = $chessboard;
+    return ($is_white_move || $is_black_move) && $same_file;
   }
 
 
   function check_diagonal_move($chessboard, $current_x, $current_y, $move_to_x, $move_to_y)
   {
     $is_takes_move = is_a($chessboard[$move_to_x][$move_to_y], "ChessPiece");
-    $is_black_move = $this->color == "black" && $move_to_x - 1 == $move_to_x;
-    $is_white_move = $this->color == "white" && $current_y + 1 == $current_y;
+    $is_black_move = $this->color == "black" && $current_y - 1 == $move_to_y;
+    $is_white_move = $this->color == "white" && $current_y + 1 == $move_to_y;
     $is_diagonal_move = abs($current_x - $move_to_x) == 1;
+    return $is_takes_move && $is_diagonal_move && ($is_black_move || $is_white_move);
+  }
 
-    return ($is_black_move || $is_white_move) && ($is_diagonal_move && $is_takes_move);
+  
+  function update_position($pos_x, $pos_y)
+  {
+    parent::update_position($pos_x, $pos_y);
+
+    if($this->check_enpassant==true){
+      $right= $this->chessboard[$pos_x+1][$pos_y];
+      $left = $this->chessboard[$pos_x-1][$pos_y];
+      if($right instanceof Pawn && $right->color != $this->color){
+        print($right);
+        $right->set_enpassant_possible();
+      }
+      
+      if($left instanceof Pawn && $left->color != $this->color){
+        $left->set_enpassant_possible();
+      }
+      $this->check_enpassant = false;
+    }
+  }
+
+  function set_enpassant_possible()
+  {
+    $this->enpassant_possible = true;
+  }
+
+  function check_enpassant($chessboard, $current_x, $current_y, $move_to_x, $move_to_y){
+    if($this->enpassant_possible){
+      if($move_to_x == $this->x+1 && $move_to_y == $this->y+1){
+        return true;
+      }
+
+
+    }
+    return false;
   }
 }
